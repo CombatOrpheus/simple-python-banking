6120e6ffa6f6b507c770dfc57dc69888a49faefa
Gabriel Santos Oliveira <gsoliveira@localhost.localdomain>
2023-08-01 18:21:27 -0300

    Reorganizing operations into functions

--- banking.py
+++ banking.py
@@ -1,3 +1,4 @@
+from typing import List, Callable
 menu = """
 
 [d] Depositar
@@ -10,13 +11,12 @@ menu = """
 saldo = 0
 limite = 500
 extrato = ""
-mumero_saques = 0
-LIMITE_SAQUES = 3
+limite_saques = 3
 
-def parse_input(parsing_function):
+def parse_input(parsing_function: Callable[[any], str]):
     value = 0
     try:
-        value = parsing_function(input())
+        value = parsing_function(input("Digite o valor da operação: R$"))
     except:
         print("Valor inválido. Por favor, utilize apenas números e '.'.")
     else:
@@ -25,48 +25,68 @@ def parse_input(parsing_function):
         return value if value != 0 else 0
 
 
-def print_no_newline(string):
-    print(string, end='')
-    
+def deposito(saldo: float, valor: float, extrato: List[str]) -> (float, str):
+    saldo += valor
+    mensagem = f'Depósito: R${valor:.2f}\n'
+    extrato += mensagem
+    print(mensagem)
+    return (saldo, extrato)
+
+
+def saque(saldo: float, valor: float, limite_operacao: int, extrato: List[str], *, limite_saques: int = limite_saques) -> (float, str, int):
+    if saldo < valor:
+        print("Saldo insuficiente")
+        return (saldo, valor, limite)
+    elif valor > limite:
+        print("Limite excede o permitido para esta operação")
+        return (saldo, valor, limite)
+    else:
+        mensagem = f'Saque: R${valor:.2f}\n'
+        extrato += mensagem
+        print(mensagem)
+        saldo -= valor 
+        print(f'Saldo atual: R${saldo:.2f}')
+        return (saldo, extrato, limite_saques - 1)
+
+
+def exibir_extrato(extrato: List[str]):
+    mensagem = '=' * 10 + "Extrato" + '=' * 10 + '\n'
+    print(mensagem)
+    print(extrato)
+    print('=' * len(mensagem))
+
+
+def alterar_limite(valor: int, extrato: str) -> (int, str):
+    limite = valor
+    mensagem = f'Alteração de limite. Novo valor: R${valor:.2f}'
+    extrato += mensagem
+    print(mensagem)
+    return (limite, extrato)
+
+def adicionar_limite_saques(limite_saques: int, adicionar: int) -> int:
+    return limite_saques + adicionar
+
 
 while True:
-
     opcao = input(menu)
     match opcao:
         case "d":
-            print("Depósito")
-            print_no_newline("Digite o valor a ser depositado: R$")
-            deposito = parse_input(float)
-            saldo += deposito
-            print(f'Saldo: R${saldo:.2f}')
-            extrato += f'Depósito: R${deposito:.2f}\n'
+            valor = parse_input(float)
+            saldo, extrato = deposito(saldo, valor, extrato)
         case "s":
-            print("Saque")
-            print_no_newline(f'Digite o valor a ser sacado, observando o limite de R${limite:.2f} por saque: R$')
-            saque = parse_input(float)
-            if (saque > limite):
-                print_no_newline("Valor acima do limite permitido. Tente novamente com um valor menor que o limite atual.")
-            elif (saque > saldo):
-                print_no_newline(f'Saldo insuficiente. Saldo atual: R${saldo:.2f}')
-            elif (LIMITE_SAQUES <= 0):
-                prinf("Limite de saques atingido")
-            else:
-                print(f'Saque realizado. Valor: R${saque:.2f}')
-                extrato += f'Saque: R${saque:.2f}\n'
-                saldo -= saque
-                print(f'Saldo atual: R${saldo:.2f}')
-                LIMITE_SAQUES -= 1
+            print(f'Limite para saques: R${limite:.2f}')
+            valor = parse_input(int)
+            saldo, extrato, limite_saques = saque(saldo, valor, limite, extrato, limite_saques = limite_saques)
         case "e":
-            print("Extrato\n")
-            print(extrato, '\n')
-            print_no_newline(f'Saldo atual: R${saldo:.2f}\nLimite de Saque atual: R${limite}')
+            exibir_extrato(extrato)
         case "l":
-            print(f'Alterar limite de saque. Limite atual: R${limite:.2f}')
-            print_no_newline("Digite o novo valor desejado: R$")
-            limite = parse_input(int)
-            print_no_newline(f'Novo limite para saques: R${limite:.2f}')
-            extrato += f'Alteração de limite: R${limite:.2f}\n'
+            valor = parse_input(int)
+            limite, extrato = alterar_limite(valor, extrato)
+        case "ls":
+            valor = parse_input(int)
+            limite_saques = adicionar_limite_saques(limite_saques, valor)
         case "q":
+            print("Finalizando programa.")
             break
         case _:
             print_no_newline("Operação inválida, por favor selecione novamente a operação desjada.")
